e98d070e366c2da4ed39b6988bd88270631f89c5
Thomas Mao <tm1813@gmail.com>
2019-12-07 10:49:06 +0800

    lesson 2 part 2

--- src/Main.kt
+++ src/Main.kt
@@ -2,11 +2,11 @@ import kotlin.browser.document
 
 fun main(args: Array<String>) {
     // lesson 1
-    println("hello world")
+    //println("hello world")
 
     // lesson 2 part 1
     val root=document.getElementById("root")
-    root!!.innerHTML="<h1>Hello World</h1>"
+    /*root!!.innerHTML="<h1>Hello World</h1>"
 
     // lesson 2 part 2
     val x = 10
@@ -19,4 +19,19 @@ fun main(args: Array<String>) {
     root.innerHTML=html
     val b1=document.getElementById("b1")
     b1!!.addEventListener("click", { println("clicked")})
+     */
+
+    // lesson 3
+    val div = document.createElement("div")
+    val h1 = document.createElement("h1")
+    val b1 = document.createElement("button")
+
+    h1.textContent="Hello 2"
+    b1.textContent="Click Me 2"
+    b1.addEventListener("click", { println("clicked 2")})
+
+    div.appendChild(h1)
+    div.appendChild(b1)
+
+    root!!.appendChild(div)
 }
